abcfde40b85bce6eb6830d8188d7da1af78a6dac
vivostanoev <vivostanoev@gmail.com>
2020-11-06 13:51:44 +0200

    add a failing test

--- TestJenkins/src/test/java/OpenBrowserTests.java
+++ TestJenkins/src/test/java/OpenBrowserTests.java
@@ -1,4 +1,6 @@
+import io.qameta.allure.Allure;
 import io.qameta.allure.junit4.DisplayName;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class OpenBrowserTests extends Base{
@@ -8,5 +10,14 @@ public class OpenBrowserTests extends Base{
     public void openBrowser()
     {
 
+    }
+
+
+    @Test
+    @DisplayName("Test expected failure")
+    public void openBrowserAndFailTest()
+    {
+        Allure.step("I want to fail the test");
+        Assert.assertTrue(false);
     }
 }
